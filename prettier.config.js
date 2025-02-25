0fced58972e9f3ff8f61b4553f8033965d3933ea
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-02-25 01:36:20 -0300

    add react router

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+    semi: false,
+    singleQuote: false,
+    bracketSameLine: true,
+    trailingComma: "es5",
+    printWidth: 100,
+    quoteProps: "consistent",
+    // plugins: ["prettier-plugin-tailwindcss"],
+  }
---
+++ react-router.config.js
@@ -0,0 +1,6 @@
+import type { Config } from "@react-router/dev/config";
+
+/** @type {import("@react-router/dev/config").Config} */
+export default {
+  ssr: false,
+} satisfies Config;
