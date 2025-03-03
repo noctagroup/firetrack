ef139093eacbc279185634cfc6f08da8506821c2
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-02 23:02:19 -0300

    add starlette config

---
+++ apps/api/src/firetrack/config.py
@@ -0,0 +1,20 @@
+from pydantic import PostgresDsn
+from starlette.config import Config
+
+config: Config = Config(env_file=".env", env_prefix="FASTAPI_")
+
+POSTGRES_DB: str = config("POSTGRES_DB", default="postgres")
+POSTGRES_USER: str = config("POSTGRES_USER", default="postgres")
+POSTGRES_PASSWORD: str = config("POSTGRES_PASSWORD", default="postgres")
+POSTGRES_HOST: str = config("POSTGRES_HOST", default="localhost")
+POSTGRES_PORT: int = config("POSTGRES_PORT", default=5432, cast=int)
+
+POSTGRES_URL: str = str(
+    PostgresDsn.build(
+        scheme="postgresql+asyncpg",
+        host=POSTGRES_HOST,
+        password=POSTGRES_PASSWORD,
+        username=POSTGRES_USER,
+        port=POSTGRES_PORT,
+    )
+)
