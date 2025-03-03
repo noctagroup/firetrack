5133c542cf242b8feeaf935f48d29a20ddd45acc
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-03 01:36:15 -0300

    shadcn ui

--- apps/www/src/root.tsx
+++ apps/www/src/root.tsx
@@ -1,11 +1,20 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router"
 
+import tailwindUrl from "~/assets/styles/tailwind.css?url"
+
+import type { Route } from "./+types/root"
+
+export const meta: Route.MetaFunction = () => [
+  { charSet: "utf-8" },
+  { name: "viewport", content: "width=device-width, initial-scale=1" },
+]
+
+export const links: Route.LinksFunction = () => [{ href: tailwindUrl, rel: "stylesheet" }]
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
--- apps/www/src/routes/_index.tsx
+++ apps/www/src/routes/_index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "~/components/ui/button"
+
 export default function Home() {
-  return <h1>hello world!</h1>
+  return <Button>hello world!</Button>
 }
