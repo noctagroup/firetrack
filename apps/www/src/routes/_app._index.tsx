5d1eb35bb7582a9209b5f4d71ea19075005a922a
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-04 14:14:00 -0300

    layout inicial com breadcrumb

--- apps/www/src/routes/_app._index.tsx
+++ apps/www/src/routes/_app._index.tsx
@@ -3,10 +3,14 @@ import { Suspense } from "react"
 import { Mapbox } from "~/components/mapbox"
 import { Skeleton } from "~/components/ui/skeleton"
 
-export default function Home() {
+export default function AppIndex() {
   return (
     <Suspense fallback={<Skeleton className="h-full" />}>
       <Mapbox />
     </Suspense>
   )
+}
+
+export const handle = {
+  breadcrumb: "Cicatrizes de Queimadas",
 }
