ce1b1804c6bc6f32a99a4f8aa4d237ea462765cd
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-04 16:28:17 -0300

    typography

---
+++ apps/www/src/components/ui/typography.tsx
@@ -0,0 +1,47 @@
+import type { VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority"
+import React from "react"
+
+import { cn } from "~/lib/utils"
+
+export interface TypographyProps
+  extends React.HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof typographyVariants> {}
+
+export const Typography = React.forwardRef<HTMLHeadingElement, TypographyProps>(
+  ({ className, variant, affects, ...props }, ref) => {
+    const Comp = variant || "p"
+
+    return (
+      <Comp
+        className={cn(typographyVariants({ variant, affects, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Typography.displayName = "Typography"
+
+export const typographyVariants = cva("text-xl", {
+  variants: {
+    variant: {
+      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg: text-5xl",
+      h2: "scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
+      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+      p: "leading-7",
+    },
+    affects: {
+      default: "",
+      lead: "text-xl text-muted-foreground",
+      large: "text-lg font-semibold",
+      small: "text-sm font-medium leading-none",
+      muted: "text-sm text-muted-foreground",
+    },
+  },
+  defaultVariants: {
+    variant: "p",
+    affects: "default",
+  },
+})
--- apps/www/src/routes/_app._index/route.tsx
+++ apps/www/src/routes/_app._index/route.tsx
@@ -3,6 +3,7 @@ import { Suspense } from "react"
 import { Mapbox } from "~/components/mapbox"
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "~/components/ui/resizable"
 import { Skeleton } from "~/components/ui/skeleton"
+import { Typography } from "~/components/ui/typography"
 
 import { RESIZABLE_HANDLE_ID } from "./constants"
 import { useResizableMapRef } from "./hooks"
@@ -16,7 +17,12 @@ export default function AppIndex() {
 
   return (
     <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={25} minSize={20} maxSize={40}></ResizablePanel>
+      <ResizablePanel className="px-6 py-8" defaultSize={25} minSize={20} maxSize={40}>
+        <Typography variant="h1">h1</Typography>
+        <Typography variant="h2">h2</Typography>
+        <Typography variant="h3">h3</Typography>
+        <Typography variant="h4">h4</Typography>
+      </ResizablePanel>
 
       <ResizableHandle id={RESIZABLE_HANDLE_ID} withHandle />
 
