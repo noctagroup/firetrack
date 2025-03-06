8a9abff5f9e7f20481a9bb346499915fa747846f
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-06 00:55:12 -0300

    user agent

--- apps/docs/main.py
+++ apps/docs/main.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 from fastapi import FastAPI, Header
 from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 
 app = FastAPI(openapi_url=None, docs_url=None, redoc_url=None)
 
@@ -11,9 +11,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 OPENAPI_PATH = os.path.join(BASE_DIR, "openapi.yaml")
 
 
+@app.get("/openapi.yaml", include_in_schema=False)
+async def openapi_yaml():
+    return FileResponse(OPENAPI_PATH, media_type="application/x-yaml")
+
+
 @app.get("/", include_in_schema=False)
-async def openapi(user_agent: Annotated[str | None, Header()] = None):
+async def openapi_ui(user_agent: Annotated[str | None, Header()] = None):
     if user_agent and "Postman" in user_agent:
-        return FileResponse(OPENAPI_PATH, media_type="application/x-yaml")
+        return RedirectResponse("/openapi.yaml")
 
     return get_swagger_ui_html(openapi_url="/openapi.yaml", title="Firetracker")
