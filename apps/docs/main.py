812a92ca46957779bd1c833be87f4ece02e4c9e0
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-06 00:27:44 -0300

    gitignore

--- apps/docs/main.py
+++ apps/docs/main.py
@@ -1,6 +1,7 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import FileResponse
 
 app = FastAPI(openapi_url=None, docs_url=None, redoc_url=None)
@@ -9,8 +10,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = os.path.join(BASE_DIR, "openapi.yaml")
 
 
-@app.get("/openapi", include_in_schema=False)
-async def get_openapi_yaml():
+@app.get("/openapi.yaml", include_in_schema=False)
+async def openapi_yaml():
     """
     Serve o arquivo openapi.yaml como resposta HTTP.
     """
@@ -18,3 +19,8 @@ async def get_openapi_yaml():
         return {"error": "Arquivo não encontrado"}
 
     return FileResponse(FILE_PATH, media_type="application/x-yaml")
+
+
+@app.get("/docs", include_in_schema=False)
+async def swagger_ui():
+    return get_swagger_ui_html(openapi_url="/openapi", title="Firetracker")
