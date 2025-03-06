e31002eb084e596d3c76e8a5ce5ec9e4407be431
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-06 00:52:57 -0300

    junta os endpoints

--- apps/docs/main.py
+++ apps/docs/main.py
@@ -1,26 +1,19 @@
 import os
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import FileResponse
 
 app = FastAPI(openapi_url=None, docs_url=None, redoc_url=None)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FILE_PATH = os.path.join(BASE_DIR, "openapi.yaml")
-
-
-@app.get("/openapi.yaml", include_in_schema=False)
-async def openapi_yaml():
-    """
-    Serve o arquivo openapi.yaml como resposta HTTP.
-    """
-    if not os.path.exists(FILE_PATH):
-        return {"error": "Arquivo não encontrado"}
-
-    return FileResponse(FILE_PATH, media_type="application/x-yaml")
+OPENAPI_PATH = os.path.join(BASE_DIR, "openapi.yaml")
 
 
 @app.get("/", include_in_schema=False)
-async def swagger_ui():
+async def openapi(user_agent: Annotated[str | None, Header()] = None):
+    if user_agent and "Postman" in user_agent:
+        return FileResponse(OPENAPI_PATH, media_type="application/x-yaml")
+
     return get_swagger_ui_html(openapi_url="/openapi.yaml", title="Firetracker")
