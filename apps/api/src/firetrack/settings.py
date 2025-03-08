277577a36f2a82d630a20f7815ba50048008119f
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-08 05:06:31 -0300

    static

--- apps/api/src/firetrack/settings.py
+++ apps/api/src/firetrack/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
