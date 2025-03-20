80327c87fde31393b0556eb0bc8f97d5d1acac1e
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-20 02:10:24 -0300

    vai

--- apps/www/src/shared/hooks/use-local-storage.ts
+++ apps/www/src/shared/hooks/use-local-storage.ts
@@ -4,13 +4,9 @@ export function useLocalStorage<T>(
   key: string,
   initialValue: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const store = React.useSyncExternalStore(
-    (onStoreChange) => {
-      window.addEventListener("storage", onStoreChange)
-      return () => window.removeEventListener("storage", onStoreChange)
-    },
-    () => getLocalStorageItem(key)
-  )
+  const getSnapshot = React.useCallback(() => getLocalStorageItem(key), [key])
+
+  const store = React.useSyncExternalStore(useLocalStorageSubscribe, getSnapshot)
 
   const setState = React.useCallback<React.Dispatch<React.SetStateAction<T>>>(
     (_nextState) => {
@@ -58,4 +54,10 @@ function setLocalStorageItem<T>(key: string, value: T) {
   const stringifiedValue = JSON.stringify(value)
   window.localStorage.setItem(key, stringifiedValue)
   dispatchStorageEvent(key, stringifiedValue)
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function useLocalStorageSubscribe<Fn extends (...args: any[]) => any>(callback: Fn) {
+  window.addEventListener("storage", callback)
+  return () => window.removeEventListener("storage", callback)
 }
