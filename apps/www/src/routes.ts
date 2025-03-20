2c9f725ce331b9d7bc4d5bf762c61773b0b1e156
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-19 23:43:34 -0300

    update

--- apps/www/src/routes.ts
+++ apps/www/src/routes.ts
@@ -1,5 +1,24 @@
+import { prefix, type RouteConfigEntry } from "@react-router/dev/routes"
 import { flatRoutes } from "@react-router/fs-routes"
 
-export default flatRoutes({
-  rootDirectory: "./fenomeno/routes",
-})
+type Routes = RouteConfigEntry[]
+type RouteConfig = { prefix: string; routes: Routes }[]
+
+const routeConfig: RouteConfig = [
+  {
+    prefix: "/fenomeno",
+    routes: await flatRoutes({
+      rootDirectory: "./fenomeno/routes",
+    }),
+  },
+  {
+    prefix: "/conta",
+    routes: await flatRoutes({
+      rootDirectory: "./conta/routes",
+    }),
+  },
+]
+
+const routes: Routes = routeConfig.flatMap((c) => prefix(c.prefix, c.routes))
+
+export default routes
