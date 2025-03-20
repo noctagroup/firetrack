d02b57a3a068e0fd00afa6088520be514bc33381
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-20 00:11:21 -0300

    redirect

--- apps/www/src/conta/routes/_conta/route.tsx
+++ apps/www/src/conta/routes/_conta/route.tsx
@@ -1,4 +1,19 @@
-import { Outlet } from "react-router"
+import { Outlet, redirect } from "react-router"
+
+import { contaOptions } from "~conta/queries"
+import { queryClient } from "~shared/lib/query/client"
+
+export async function clientLoader() {
+  try {
+    const minhaConta = await queryClient.ensureQueryData(contaOptions.minhaConta())
+
+    if (minhaConta.is_authenticated) {
+      return redirect("/fenomeno")
+    }
+  } catch {
+    // noop
+  }
+}
 
 export default function Conta() {
   return <Outlet />
