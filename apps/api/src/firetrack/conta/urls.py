669ed5bd8cbeabb4989ac86172ba55c17094b2f6
otavioabreu27 <otavio.abreu96@gmail.com>
2025-05-13 11:50:33 -0300

    resolvendo bug conta

--- apps/api/src/firetrack/conta/urls.py
+++ apps/api/src/firetrack/conta/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from firetrack.conta import views
 
 urlpatterns = [
-    path("", views.conta),
-    path("entrar", views.entrar),
-    path("sair", views.sair),
-    path("cadastrar", views.cadastrar),
+    path("/", views.conta),
+    path("entrar/", views.entrar),
+    path("sair/", views.sair),
+    path("cadastrar/", views.cadastrar),
 ]
