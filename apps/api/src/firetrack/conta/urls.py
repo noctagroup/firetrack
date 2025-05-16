7e5a3a7fa7c9dae71300510b1c4d5e6b66263460
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-05-16 19:57:43 -0300

    fix: https://{BASE_URL}/conta dá 404, se não tiver // no final

--- apps/api/src/firetrack/conta/urls.py
+++ apps/api/src/firetrack/conta/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from firetrack.conta import views
 
 urlpatterns = [
-    path("/", views.conta),
+    path("", views.conta),
     path("entrar/", views.entrar),
     path("sair/", views.sair),
     path("cadastrar/", views.cadastrar),
