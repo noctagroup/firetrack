24678aa3759a779096e0941e09c0e6a5f097ab43
biamontanini <127904901+BiaMontanini@users.noreply.github.com>
2025-06-10 21:02:50 -0300

    componente sem detecção

--- apps/www/src/deteccao/routes/deteccao._index/route.tsx
+++ apps/www/src/deteccao/routes/deteccao._index/route.tsx
@@ -1,1 +1,20 @@
-export default function DeteccaoIndex() {}
+import { CircleOff } from "lucide-react"
+import { Button } from "~shared/lib/shadcn/ui/button"
+
+export default function DeteccaoIndex() {
+  return (
+    <div className="flex min-h-screen items-center justify-center px-4">
+      <div className="w-full max-w-md space-y-6 text-center">
+        <CircleOff className="mx-auto" size={48} />
+        <h1 className="text-3xl leading-none font-semibold">Nenhuma detecção criada</h1>
+        <h2 className="text-muted-foreground text-md">
+          Para detectar cicatrizes de queimadas crie uma nova detecção
+        </h2>
+
+        <div className="flex justify-center">
+          <Button className="w-40">Criar detecção</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
