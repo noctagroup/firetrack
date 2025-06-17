5046b803b4f5fec131d48589595802a743c93558
otavioabreu27 <otavio.abreu96@gmail.com>
2025-06-17 20:50:35 -0300

    change for deploy

--- apps/api/src/firetrack/produtos/serializers.py
+++ apps/api/src/firetrack/produtos/serializers.py
@@ -3,7 +3,7 @@ from firetrack.produtos.models import Produto
 
 def serialize_produto(produto: Produto) -> dict:
     return {
-        "product_id": produto.product_id,
+        "id": produto.product_id,
         "description": produto.description,
         "regrowth_threshold": produto.regrowth_threshold,
     }
